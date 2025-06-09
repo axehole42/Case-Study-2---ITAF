c2bd745c79b38436d7a1502c7c9e2ff6c22b7b94
axehole42 <112853835+axehole42@users.noreply.github.com>
2025-06-09 09:35:22 +0200

    Update Python Code_v2_Group 9_Case Study 1_Efficient Frontier, CML, MVP, TP.py

--- Python Code_v2_Group 9_Case Study 1_Efficient Frontier, CML, MVP, TP.py
+++ Python Code_v2_Group 9_Case Study 1_Efficient Frontier, CML, MVP, TP.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 16 16:58:24 2024
-
-@author: Viktor Tsonevski
-
 
 """
 'This version of the code includes the Optimal Risky Portfolio, I ran this in Google Collab, might not work in Spyder'
@@ -23,7 +17,7 @@ import plotly.io as pio
 import os
 import kaleido
 
-os.chdir(r"C:\Users\Viktor\Desktop\UNI MUNSTER 2024-2026\Introduction to Advanced Finance\Case Study 1\")
+os.chdir(r"C:\Users\Desktop")
 
 
 'List of stock tickers'
